8cbdcf2696d60ab9070f87988c028c2b6684db67
moops <pro100vaness@gmail.com>
2021-04-19 00:47:39 +1000

    19.04

--- pages/catalog.js
+++ pages/catalog.js
@@ -1,7 +1,7 @@
 import MainContainer from "../components/MainContainer";
 import A from "../components/A";
 
-import styles from "../styles/catalog.module.sass";
+// import styles from "../styles/catalog.module.sass";
 
 const Catalog = ({}) => {
   return (
